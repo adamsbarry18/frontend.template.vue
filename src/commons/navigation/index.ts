696c911e39b067394b31857399af0d1c165c6971
Barry Mamadou Adama <mamadou-adama.barry@etudiant.univ-rennes1.fr>
2025-02-21 17:07:02 +0100

    feat(navigation): imports

--- src/commons/navigation/index.ts
+++ src/commons/navigation/index.ts
@@ -2,5 +2,14 @@ import UTabTitle from '@/commons/navigation/UTabTitle.vue';
 import UWizardNav from '@/commons/navigation/UWizardNav.vue';
 import UExplorer from '@/commons/navigation/UExplorer.vue';
 import UVerticalSubStepper from '@/commons/navigation/UVerticalSubStepper.vue';
+import UVerticalStepper from '@/commons/navigation/UVerticalStepper.vue';
+import UWiredVerticalStepper from '@/commons/navigation/UWiredVerticalStepper.vue';
 
-export { UTabTitle, UWizardNav, UExplorer, UVerticalSubStepper };
+export {
+  UTabTitle,
+  UWizardNav,
+  UExplorer,
+  UVerticalSubStepper,
+  UVerticalStepper,
+  UWiredVerticalStepper,
+};
