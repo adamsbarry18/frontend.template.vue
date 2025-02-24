59b43f388e8f307cf9fc4bc0ebc4e44da386e6a4
Barry Mamadou Adama <mamadou-adama.barry@etudiant.univ-rennes1.fr>
2025-02-24 10:05:44 +0100

    feat(layout): add grid card

--- src/commons/layout/index.ts
+++ src/commons/layout/index.ts
@@ -1,3 +1,6 @@
 import UCard from '@/commons/layout/UCard.vue';
+import UGrid from '@/commons/layout/UGrid.vue';
+import UCatalogueCard from '@/commons/layout/UCatalogueCard.vue';
+import UDashboardHeaderCard from '@/commons/layout/UDashboardHeaderCard.vue';
 
-export { UCard };
+export { UCard, UCatalogueCard, UGrid, UDashboardHeaderCard };
