e3d9ace86a72880561bacd80bb304f363aa6a344
Barry Mamadou Adama <mamadou-adama.barry@etudiant.univ-rennes1.fr>
2025-05-07 17:57:27 +0200

    feat: fix vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,26 @@
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import dns from 'dns';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '');
+const proxyTarget = process.env.API_URL || 'https://localhosht:8000';
 
+dns.setDefaultResultOrder('verbatim');
+
+export default defineConfig(({ mode }) => {
+  const isDevBuild = mode === 'development';
   return {
     plugins: [vue()],
-    server: {
-      port: 8080,
-    },
     resolve: {
       dedupe: ['@storybook/client-api'],
       alias: {
         '@': path.resolve(path.dirname(fileURLToPath(import.meta.url)), './src'),
       },
+      extensions: ['.js', '.json', '.vue', '.ts'],
     },
     build: {
+      sourcemap: isDevBuild ? true : 'hidden',
       lib: {
         entry: path.resolve(path.dirname(fileURLToPath(import.meta.url)), 'src/index.ts'),
         name: 'frontend.template.vue3',
@@ -39,14 +42,36 @@ export default defineConfig(({ mode }) => {
         },
       },
     },
+    server: {
+      port: 8080,
+      proxy: {
+        '/api': {
+          target: proxyTarget,
+          changeOrigin: true,
+          cookieDomainRewrite: {
+            '*': '',
+          },
+        },
+      },
+    },
+    preview: {
+      port: 8080,
+    },
     esbuild: {
       keepNames: true,
     },
     test: {
       globals: true,
-      environment: 'jsdom',
-      env: {
-        VITE_API_URL: env.VITE_API_URL || 'http://localhost:8000/api/v1',
+      isolate: true,
+      environment: 'happy-dom',
+      reporters: ['default', 'junit', 'html'],
+      outputFile: {
+        junit: './junit.xml',
+      },
+      coverage: {
+        include: ['src/**'],
+        provider: 'istanbul',
+        reporter: ['text', 'cobertura', 'html', 'lcovonly'],
       },
     },
   };
